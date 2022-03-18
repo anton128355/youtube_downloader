a68e6df86d617520b278ae2faf4912c21c1dae93
Anton <anton128355@gmail.com>
2022-03-18 19:35:30 +0300

    Refactoring

--- main.py
+++ main.py
@@ -4,24 +4,19 @@ import youtube_dl
 app = flask.Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
-def get_info_about_video(): 
-    global lst_variants_of_content, dict_variants_of_content
+def post_video_url(): 
+    global dict_of_content
     ydl_opts = {}
-    json_data = None
-    lst_variants_of_content, dict_variants_of_content = [], dict()
+    dict_of_content = dict()
     if flask.request.method == "POST":
         video_url = flask.request.form.get("video_url")
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             json_data = ydl.extract_info(video_url, download=False)
-    
         for content in json_data["formats"]:
             if content["width"] == None:
-                lst_variants_of_content.append(f"audio-{content['ext']}")
-                dict_variants_of_content.update({f"audio-{content['ext']}": content['url']})
+                dict_of_content.update({f"audio-{content['ext']}": content['url']})
             elif content["width"] != None:
-                lst_variants_of_content.append(f"video-{content['ext']}-{content['width']}")
-                dict_variants_of_content.update({f"video-{content['ext']}-{content['width']}": content['url']})
-        
+                dict_of_content.update({f"video-{content['ext']}-{content['width']}": content['url']})
         return flask.redirect("/additional_data")
     else:
         return flask.render_template("main_page.html")
@@ -29,10 +24,9 @@ def get_info_about_video():
 @app.route("/additional_data", methods=['GET', 'POST'])
 def additional_data():
     if flask.request.method == "POST":
-        result = flask.request.form.get("variants_of_video_and_audio")
-        return flask.redirect(dict_variants_of_content[result])
+        return flask.redirect(dict_of_content[flask.request.form.get("list_of_contents")])
     else:
-        return flask.render_template("download_data.html", lst_variants_of_content=lst_variants_of_content)
+        return flask.render_template("download_data.html", dict_of_content=dict_of_content.keys())
 
 if __name__ == "__main__":
     app.run(debug=True)
